bc1e4acfd1fc14909c4fc6ae210b3ea6918da5a7
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-01-24 16:53:16 +0530

    Arweave bundler instance with fund created

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,49 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { MainContext } from "../context";
+import { providers, utils } from "ethers";
+import { WebBundlr } from "@bundlr-network/client";
+import { useState, useEffect, useRef } from "react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [bundlrInstance, setBundlrInstance] = useState();
+  const bundlrRef = useRef();
+  const [balance, setBalance] = useState();
+
+  async function initialize() {
+    await window.ethereum.enable();
+
+    const provider = new providers.Web3Provider(window.ethereum);
+    await provider._ready();
+
+    const bundlr = new WebBundlr(
+      "https://devnet.bundlr.network",
+      "matic",
+      provider,
+      {
+        providerUrl: "https://rpc-mumbai.matic.today",
+      }
+    );
+    await bundlr.ready();
+    console.log(bundlr);
+    setBundlrInstance(bundlr);
+    bundlrRef.current = bundlr;
+    fetchBalance();
+    return bundlr; // done!
+  }
+
+  async function fetchBalance() {
+    const bal = await bundlrRef.current.getLoadedBalance();
+    console.log("bal: ", utils.formatEther(bal.toString()));
+    setBalance(utils.formatEther(bal.toString()));
+  }
+
+  return (
+    <div>
+      <MainContext.Provider
+        value={{ initialize, bundlrInstance, bundlrRef, balance, fetchBalance }}
+      >
+        <Component {...pageProps} />
+      </MainContext.Provider>
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,67 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "../styles/Home.module.css";
+import { providers, utils } from "ethers";
+import { WebBundlr } from "@bundlr-network/client";
+import { useState, useEffect, useRef, useContext } from "react";
+import { MainContext } from "../context";
+import BigNumber from "bignumber.js";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [file, setFile] = useState();
+  const [image, setImage] = useState();
+  const [URI, setURI] = useState();
+  const [amount, setAmount] = useState();
+
+  const { initialize, bundlrInstance, bundlrRef, balance, fetchBalance } =
+    useContext(MainContext);
+
+  async function fundWallet() {
+    if (!amount) return;
+    const parsedAmount = new BigNumber(amount).multipliedBy(
+      bundlrInstance.currencyConfig.base[1]
+    );
+    console.log(parsedAmount, amount);
+    if (parsedAmount > 1) {
+      const response = await bundlrInstance.fund(parsedAmount);
+      console.log("Wallet Funded : ", response);
+      fetchBalance();
+    }
+  }
+
+  async function uploadFile() {
+    console.log(file);
+    let tx = await bundlrInstance.uploader.chunkedUploader.uploadData(file, [
+      { name: "Content-type", value: "image/png" },
+    ]);
+    console.log(tx);
+    setURI(`https://arweave.net/${tx.data.id}`);
+  }
+
+  async function uploadFile2() {
+    console.log(file);
+    let tx = await bundlrInstance.upload(file);
+    console.log(tx);
+    setURI(`https://arweave.net/${tx.data.id}`);
+  }
+
+  function onFileChange(e) {
+    const file = e.target.files[0];
+    if (file) {
+      const image = URL.createObjectURL(file);
+      setImage(image);
+      let reader = new FileReader();
+      reader.onload = function () {
+        if (reader.result) {
+          setFile(Buffer.from(reader.result));
+        }
+      };
+    }
+  }
+
   return (
     <>
       <Head>
@@ -16,108 +72,65 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
+          <p>Get started by intializing the bundlr</p>
           <div>
             <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              href="https://twitter.com/0xdhruva"
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
+              By @0xdhruva
             </a>
           </div>
         </div>
 
         <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
+          <div>
+            {balance && <h3 className={styles.button}>Balance : {balance}</h3>}
           </div>
-        </div>
+          {!bundlrInstance ? (
+            <button className={styles.button} onClick={initialize}>
+              Initialize bundlr
+            </button>
+          ) : (
+            <>
+              <div>
+                <input
+                  className={styles.input}
+                  onChange={(e) => {
+                    setAmount(e.target.value);
+                  }}
+                  placeholder="Amout to fund wallet"
+                ></input>
+              </div>
+              <h1 className={styles.button}>Bundlr Intialized</h1>
+              <button className={styles.button} onClick={fundWallet}>
+                Fund Wallet
+              </button>
+              <div>
+                <input
+                  className={styles.inputFile}
+                  onChange={(e) => {
+                    onFileChange(e);
+                  }}
+                  type="file"
+                  placeholder="Amout to fund wallet"
+                ></input>
+              </div>
+              <button className={styles.button} onClick={uploadFile}>
+                Upload File
+              </button>
 
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+              {URI && (
+                <a href={URI} target="_blank" rel="noopener noreferrer">
+                  {URI}
+                </a>
+              )}
+              {image && <img src={image} style={{ width: "500px" }} />}
+            </>
+          )}
         </div>
       </main>
     </>
-  )
+  );
 }
